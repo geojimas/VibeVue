0ad0dd197cfb4e0078dafdf89216c8de2b4dd7c5
Dimitris Georgiadis <geojimas@gmail.com>
2025-05-24 21:15:36 +0300

    add sitemap for SEO

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), Sitemap({ hostname: 'https://vptdes.netlify.app' })],
+
 })
