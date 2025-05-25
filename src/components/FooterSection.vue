0c0f4277fe9504f08702f51e16df2518a4d1597b
Dimitris Georgiadis <geojimas@gmail.com>
2025-05-25 14:03:29 +0300

    footer added

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+
+<template>
+  <div class="text-sm p-4 text-center">
+    Copyright &#169; {{ new Date().getFullYear() }} by Dimitris Georgiadis, All Rights reserved.
+  </div>
+</template>
