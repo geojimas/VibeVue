ba255273b13a8cc4ba6f91638bc2458a927913ba
Dimitris Georgiadis <geojimas@gmail.com>
2025-05-25 04:02:29 +0300

    remove footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
   import { RouterView } from 'vue-router'
   import HeaderSection from './components/HeaderSection.vue'
-  import FooterSection from './components/FooterSection.vue'
 </script>
 
 <template>
   <div class="flex flex-col h-screen">
     <HeaderSection />
     <RouterView />
-    <FooterSection />
   </div>
 </template>
 
