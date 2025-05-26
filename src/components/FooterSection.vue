5e1552581a12dcce9e7732ac20af953e42b26a57
Dimitris Georgiadis <geojimas@gmail.com>
2025-05-27 00:11:18 +0300

    change icon

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-xs p-4 text-center">
+  <div class="text-xs p-2 text-center">
     {{ $t('footer.header') }} &#169; {{ new Date().getFullYear() }} {{ $t('footer.label') }}
   </div>
 </template>
