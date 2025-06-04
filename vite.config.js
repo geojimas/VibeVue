1007846e97829c28daec6b160d0f28004de3b915
Dimitris Georgiadis <geojimas@gmail.com>
2025-06-04 23:35:32 +0300

    pwa configs

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,37 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     analyzer(),
-    VitePWA(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      mode: 'production',
+      injectRegister: 'auto',
+      workbox: {
+        cleanupOutdatedCaches: false,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'Vibe Vue',
+        short_name: 'VibeVue',
+        description: 'vue 3 Template',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon.svg',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    }),
     Sitemap({ hostname: 'https://vibe-vue.vercel.app' }),
   ],
 })
